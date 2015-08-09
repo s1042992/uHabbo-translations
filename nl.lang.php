efeb6c7147ed64d198aa6754a8b6902b3075701a
Xesau <aron2000@zeelandnet.nl>
2015-08-09 21:33:22 +0200

    Languages. Some may lack stuff/be not translated at all

---
+++ nl.lang.php
@@ -0,0 +1,64 @@
+<?php
+
+$lang = [
+    'translation.language' => 'Nederlands',
+    'translation.translator' => 'Xesau',
+    'translation.translator.url' => 'https://uhabbo.xesau.eu',
+    'translation.last-updated' => 'August 3th 2015',
+
+    'error.page-not-found' => 'Pagina niet gevonden',
+    'error.page-not-found.description' => 'De opgevraagde pagina kon niet worden gevonden.',
+    'error.no-access' => 'Geen toegang',
+    'error.no-access.description' => 'Je hebt toegang (meer) tot deze pagina.',
+    'error.return-to-home' => 'Keer terug naar de homepagina',
+
+    'error.login.login' => 'Er is geen account met deze gebruikersnaam of dit e-mailadres',
+    'error.login.password' => 'Het ingevoerde wachtwoord klopt niet',
+    'error.login.banned' => 'Dit account is verbannen. Reden: %1',
+    'error.login.blocked' => 'De inlogpoging werd geblokkeerd.',
+
+    'error.register.username.legnth' => 'Je gebruikersnaam moet minstens 3 tekens en maximaal 20 tekens lang zijn.',
+    'error.register.username.not-available' => 'Deze gebruikersnaam is niet meer beschikbaar',
+    'error.register.password.length' => 'Je wachtwoord moet minstens 8 tekens lang zijn',
+    'error.register.email.not-available' => 'Er is al een account met dit e-mailadres',
+    'error.register.email.format' => 'Dit e-mailadres is niet goed.',
+
+    'general.advertisement' => 'Advertentie',
+    'general.users-online' => 'Er zijn %1 spelers online!',
+    'general.welcome' => 'Welkom op %1',
+    'general.register-now' => 'Registreer je nu op %1',
+    'general.enter-hotel' => 'Ga in %1',
+    'general.logged-out' => 'Je bent uitgelogd! Tot ziens.',
+    'general.welcome-back' => 'Welkom terug, %1',
+
+    'login' => 'Inloggen',
+    'login.username-or-email' => 'Gebruikersnaam of e-mail',
+    'login.password' => 'Wachtwoord',
+    'login.remember-me' => 'Onthoud mij',
+    'login.submit' => 'Inloggen!',
+
+    'register' => 'Registreren',
+    'register.username' => 'Gebruikersnaam',
+    'register.email' => 'E-mailadres',
+    'register.password' => 'Wachtwoord',
+    'register.password-repeat' => 'Wachtwoord (herhalen)',
+    'register.captcha' => 'Ik ben geen robot:',
+    'register.submit' => 'Registreren!',
+    'register.account-created' => 'Gefeliciteerd! Je account is aangemaakt. Controller je e-mail op het activatiebericht. TIP: Kijk ook in je spam/junk.',
+    'register.account-created.title' => 'Je account op %1 is aangemaakt',
+    'register.account-created.message' => "Beste %1,\r\n\r\nJe account op %3 is aangemaakt.\r\nVoordat je kunt inloggen, moet je account nog geactiveerd worden. Zo weten we zeker"
+            . " dat dit e-mailadres geldig is. Je kunt je account activeren door op de volgende link te klikken:\r\n\%2\r\n\r\nMet vriendelijke groet\r\nHet team van %3",
+
+    'news' => 'Nieuws',
+    'news.latest-news' => 'Het laatste nieuws',
+    'news.select-article' => 'Selecteer een artikel uit de lijst om te lezen',
+    'news.read-more' => 'Lees meer',
+    'news.article.published' => 'Artikel gepubliceerd op %2 door %1',
+
+    'user.last-online' => 'Laatst online op %1',
+
+    'community' => 'Gemeenschap',
+    'community.staff' => 'Medewerkers',
+    'community.staff.staff-on-hotel' => 'Staff op %1',
+    // 'community.staff.staff-on-hotel.description' => ''
+];
